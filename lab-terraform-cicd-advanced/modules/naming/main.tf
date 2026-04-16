dad7a3c7afa59dc2f4a1f2870b488e37d09ab654
Elel94 <elias.derghaoui@social.aston-ecole.com>
2026-04-16 12:57:02 +0200

    Initial clean import project

---
+++ lab-terraform-cicd-advanced/modules/naming/main.tf
@@ -0,0 +1,12 @@
+locals {
+  naming_convention = {
+    for app in var.applications :
+    app.name => {
+      logical_name = "${var.project_name}-${var.environment}-${app.name}"
+      short_name   = "${var.environment}-${app.name}"
+      tier_code    = substr(app.tier, 0, 3)
+      owner_team   = app.team
+      port         = app.port
+    }
+  }
+}
